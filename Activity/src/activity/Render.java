109a59202012ba1a4e9506d8bb2798efd626df99
ForceTen2112 <SguitarAdragonM@gmail.com>
2014-12-02 00:04:44 -0500

    added whatever. probably wont use it

--- Activity/src/activity/Render.java
+++ Activity/src/activity/Render.java
@@ -1,8 +1,11 @@
 package activity;
 
 import java.awt.*;
+import java.awt.image.BufferStrategy;
+import java.awt.image.BufferedImage;
+import java.awt.image.DataBufferInt;
 
-public class Render {
+public class Render extends Canvas{
 	
 	public final static int TILE_SIZE = 9; // MUST BE A MULTIPLE OF 3
 	public final static Color WALL_COLOR = Color.black;
@@ -11,15 +14,15 @@ public class Render {
 	public final static int WALL = 0x000000;
 	public final static int PATH = 0x7f7f7f;
 	public final static int WALKER = 0xffffff;
+	
+	public static int buffer = 3;
 	private Maze maze;
 	private int[][] pixels;
-	private int[][] pixelsToShow;
 	//private int currentX, currentY;
 	
 	public Render(Maze maze) {
 		this.maze = maze;
 		pixels = new int[maze.cols * TILE_SIZE][maze.rows * TILE_SIZE];
-		pixelsToShow = new int[maze.cols * TILE_SIZE][maze.rows * TILE_SIZE];
 		fillImage();
 		generateImage();
 		//currentX = 0;
@@ -73,14 +76,34 @@ public class Render {
 	}
 	
 	public void renderImage(int currentX, int currentY) {
-		pixelsToShow = pixels.clone();
+		BufferedImage image = new BufferedImage(maze.cols * TILE_SIZE, maze.rows * TILE_SIZE, BufferedImage.TYPE_INT_RGB);
+		int[] pixelsToShow = ((DataBufferInt)image.getRaster().getDataBuffer()).getData();
+		
+		int [][] pixelsToShowMatrix = pixels.clone();
 		for (int x = currentX + (TILE_SIZE / 3); x < currentX + 2 * (TILE_SIZE / 3); x++)
 			for (int y = currentY + (TILE_SIZE / 3); y < currentY + 2 * (TILE_SIZE / 3); y++)
-				pixelsToShow[x][y] = WALKER;
+				pixelsToShowMatrix[x][y] = WALKER;
 		
+		for (int x = 0; x < pixelsToShowMatrix.length; x++)
+			for (int y = 0; y < pixelsToShowMatrix[0].length; y++)
+				pixelsToShow[x + pixelsToShowMatrix.length * y] = pixelsToShowMatrix[x][y];
+		
+		BufferStrategy bs = getBufferStrategy();
+		if (bs == null) {
+			createBufferStrategy(buffer);
+			return;
+		}
+		
+		Graphics g = bs.getDrawGraphics();
+		g.setColor(WALL_COLOR);
+		g.fillRect(0, 0, getWidth(), getHeight());
+		g.drawImage(image, 0, 0, getWidth(), getHeight(), null);
+		//g.fillRect(Mouse.getX(), Mouse.getY(), 64, 64);
+		g.dispose();
+		bs.show();
 	}
 	
-	public void paint(Graphics g) {
+	/*public void paint(Graphics g) {
 		for (int x = 0; x < maze.cols; x++) {
 			for (int y = 0; y < maze.rows; y++) {
 				g.setColor(WALL_COLOR);
@@ -103,5 +126,8 @@ public class Render {
 				//g.fillRect(x * TILE_SIZE + (TILE_SIZE / 3), y * TILE_SIZE + (TILE_SIZE / 3), TILE_SIZE / 3, TILE_SIZE / 3);
 			}
 		}
-	}
+		
+	}*/
+	
+	
 }
