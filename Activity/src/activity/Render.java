a65658dfabafc7e5a853b6dc7cdf3529eae54974
ForceTen2112 <SguitarAdragonM@gmail.com>
2014-12-01 22:58:23 -0500

    render now also has a matrix of pixels

--- Activity/src/activity/Render.java
+++ Activity/src/activity/Render.java
@@ -1,25 +1,83 @@
 package activity;
 
 import java.awt.*;
-import java.applet.*;
 
-public class Render extends Applet {
+public class Render {
 	
-	private Maze maze;
 	public final static int TILE_SIZE = 9; // MUST BE A MULTIPLE OF 3
 	public final static Color WALL_COLOR = Color.black;
 	public final static Color PATH_COLOR = Color.gray;
 	public final static Color WALKER_COLOR = Color.white;
-	private int currentX, currentY;
+	public final static int WALL = 0x000000;
+	public final static int PATH = 0x7f7f7f;
+	public final static int WALKER = 0xffffff;
+	private Maze maze;
+	private int[][] pixels;
+	private int[][] pixelsToShow;
+	//private int currentX, currentY;
 	
-	public Render(Maze maze, int currentX, int currentY) {
+	public Render(Maze maze) {
 		this.maze = maze;
-		this.currentX = currentX;
-		this.currentY = currentY;
+		pixels = new int[maze.cols * TILE_SIZE][maze.rows * TILE_SIZE];
+		pixelsToShow = new int[maze.cols * TILE_SIZE][maze.rows * TILE_SIZE];
+		fillImage();
+		generateImage();
+		//currentX = 0;
+		//currentY = 0;
 	}
 	
-	public void init() {
-		setBackground(Color.BLACK);
+	/*public Render(Maze maze, int currentX, int currentY) {
+		this.maze = maze;
+		pixels = new int[maze.cols * TILE_SIZE][maze.rows * TILE_SIZE];
+		pixelsToShow = new int[maze.cols * TILE_SIZE][maze.rows * TILE_SIZE];
+		fillImage();
+		generateImage();
+		this.currentX = currentX;
+		this.currentY = currentY;
+	}*/
+	
+	public void fillImage() {
+		for (int x = 0; x < pixels.length; x++)
+			for (int y = 0; y < pixels[0].length; y++)
+				pixels[x][y] = WALL;
+	}
+	
+	public void generateImage() {
+		for (int tileX = 0; tileX < maze.cols; tileX++) {
+			for (int tileY = 0; tileY < maze.rows; tileY++) {
+				for (int x = 0; x < TILE_SIZE; x++) {
+					for (int y = 0; y < TILE_SIZE; y++) {
+						if ((x < 3 || x >= 6) && (y < 3 || y >= 6));
+						else if ((x < 3) && (y >= 3 || y < 6)) {//LEFT
+							if (!maze.rooms[tileX][tileY].doors[0].isWall)
+								pixels[x + (tileX * TILE_SIZE)][y + (tileY * TILE_SIZE)] = PATH;
+						}
+						else if ((x >= 6) && (y >= 3 || y < 6)) {//RIGHT
+							if (!maze.rooms[tileX][tileY].doors[1].isWall)
+								pixels[x + (tileX * TILE_SIZE)][y + (tileY * TILE_SIZE)] = PATH;
+						}
+						else if ((x >= 3 || x < 6) && (y < 3)) {//UP
+							if (!maze.rooms[tileX][tileY].doors[2].isWall)
+								pixels[x + (tileX * TILE_SIZE)][y + (tileY * TILE_SIZE)] = PATH;
+						}
+						else if ((x >=3 || x < 6) && (y >= 6)) {//DOWN
+							if (!maze.rooms[tileX][tileY].doors[3].isWall)
+								pixels[x + (tileX * TILE_SIZE)][y + (tileY * TILE_SIZE)] = PATH;
+						}
+						else
+							pixels[x + (tileX * TILE_SIZE)][y + (tileY * TILE_SIZE)] = PATH;
+					}
+				}
+			}
+		}
+	}
+	
+	public void renderImage(int currentX, int currentY) {
+		pixelsToShow = pixels.clone();
+		for (int x = currentX * TILE_SIZE; x < TILE_SIZE; x++)
+			for (int y = currentY * TILE_SIZE; y < TILE_SIZE; y++)
+				pixelsToShow[x][y] = WALKER;
+		
 	}
 	
 	public void paint(Graphics g) {
@@ -40,8 +98,8 @@ public class Render extends Applet {
 					g.fillRect(x * TILE_SIZE + (TILE_SIZE / 3), y * TILE_SIZE, TILE_SIZE, TILE_SIZE);
 				if (!maze.rooms[x][y].doors[3].isWall) //DOWN
 					g.fillRect(x * TILE_SIZE + (TILE_SIZE / 3), y * TILE_SIZE + (2 * (TILE_SIZE / 3)), TILE_SIZE, TILE_SIZE);
-				if (currentX == x && currentY == y)
-					g.setColor(WALKER_COLOR);
+				//if (currentX == x && currentY == y)
+				//	g.setColor(WALKER_COLOR);
 				g.fillRect(x * TILE_SIZE + (TILE_SIZE / 3), y * TILE_SIZE + (TILE_SIZE / 3), TILE_SIZE / 3, TILE_SIZE / 3);
 			}
 		}
