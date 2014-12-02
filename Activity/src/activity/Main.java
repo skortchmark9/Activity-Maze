99ee370053cc92f061b6179704bead1ad52dde13
skortchmark9 <samuel_kortchmar@brown.edu>
2014-12-02 04:30:34 -0500

    init solver correctly in main

--- Activity/src/activity/Main.java
+++ Activity/src/activity/Main.java
@@ -19,7 +19,7 @@ public class Main {
 		
 		Maze m = new Maze("mazes/maze0.txt", 10, 25);
 //		m.printMaze();
-		Solver s = new Solver(m);
+		Solver s = new Solver(m, 0, 0, 24, 9);
 		
 		
 		new Frontend(s);
