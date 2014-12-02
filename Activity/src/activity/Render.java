560b6b7abb259d661d7100c99fcff8ffe4bee1a8
skortchmark9 <samuel_kortchmar@brown.edu>
2014-12-02 01:23:55 -0500

    Reverted render

--- Activity/src/activity/Render.java
+++ Activity/src/activity/Render.java
@@ -1,28 +1,27 @@
 package activity;
 
-import java.awt.*;
-import java.awt.image.BufferStrategy;
-import java.awt.image.BufferedImage;
-import java.awt.image.DataBufferInt;
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
 
-public class Render extends Canvas{
+public class Render {
 	
-	public final static int TILE_SIZE = 9; // MUST BE A MULTIPLE OF 3
+	public final static int TILE_SIZE = 9 * 7; // MUST BE A MULTIPLE OF 3
 	public final static Color WALL_COLOR = Color.black;
 	public final static Color PATH_COLOR = Color.gray;
 	public final static Color WALKER_COLOR = Color.white;
 	public final static int WALL = 0x000000;
 	public final static int PATH = 0x7f7f7f;
 	public final static int WALKER = 0xffffff;
-	
-	public static int buffer = 3;
-	private Maze maze;
+	public Maze maze;
 	private int[][] pixels;
+	private int[][] pixelsToShow;
 	//private int currentX, currentY;
 	
 	public Render(Maze maze) {
 		this.maze = maze;
 		pixels = new int[maze.cols * TILE_SIZE][maze.rows * TILE_SIZE];
+		pixelsToShow = new int[maze.cols * TILE_SIZE][maze.rows * TILE_SIZE];
 		fillImage();
 		generateImage();
 		//currentX = 0;
@@ -76,58 +75,39 @@ public class Render extends Canvas{
 	}
 	
 	public void renderImage(int currentX, int currentY) {
-		BufferedImage image = new BufferedImage(maze.cols * TILE_SIZE, maze.rows * TILE_SIZE, BufferedImage.TYPE_INT_RGB);
-		int[] pixelsToShow = ((DataBufferInt)image.getRaster().getDataBuffer()).getData();
-		
-		int [][] pixelsToShowMatrix = pixels.clone();
+		pixelsToShow = pixels.clone();
 		for (int x = currentX + (TILE_SIZE / 3); x < currentX + 2 * (TILE_SIZE / 3); x++)
 			for (int y = currentY + (TILE_SIZE / 3); y < currentY + 2 * (TILE_SIZE / 3); y++)
-				pixelsToShowMatrix[x][y] = WALKER;
+				pixelsToShow[x][y] = WALKER;
 		
-		for (int x = 0; x < pixelsToShowMatrix.length; x++)
-			for (int y = 0; y < pixelsToShowMatrix[0].length; y++)
-				pixelsToShow[x + pixelsToShowMatrix.length * y] = pixelsToShowMatrix[x][y];
-		
-		BufferStrategy bs = getBufferStrategy();
-		if (bs == null) {
-			createBufferStrategy(buffer);
-			return;
-		}
-		
-		Graphics g = bs.getDrawGraphics();
-		g.setColor(WALL_COLOR);
-		g.fillRect(0, 0, getWidth(), getHeight());
-		g.drawImage(image, 0, 0, getWidth(), getHeight(), null);
-		//g.fillRect(Mouse.getX(), Mouse.getY(), 64, 64);
-		g.dispose();
-		bs.show();
 	}
 	
-	/*public void paint(Graphics g) {
+	public void paint(Graphics g) {
+		g.setColor(WALL_COLOR);
+		g.fillRect(0, 0, TILE_SIZE * maze.cols, TILE_SIZE * maze.rows);
 		for (int x = 0; x < maze.cols; x++) {
 			for (int y = 0; y < maze.rows; y++) {
 				g.setColor(WALL_COLOR);
 				g.fillRect(x * TILE_SIZE, y * TILE_SIZE, TILE_SIZE, TILE_SIZE);
+				Graphics2D g2d = (Graphics2D) g;
+
 				//g.drawRect(x * TILE_SIZE, y * TILE_SIZE, TILE_SIZE / 3, TILE_SIZE / 3);
 				//g.drawRect(x * TILE_SIZE + (2 * (TILE_SIZE / 3)), y * TILE_SIZE, TILE_SIZE / 3, TILE_SIZE / 3);
 				//g.drawRect(x * TILE_SIZE + (2 * (TILE_SIZE / 3)), y * TILE_SIZE + (2 * (TILE_SIZE / 3)), TILE_SIZE / 3, TILE_SIZE / 3);
 				//g.drawRect(x * TILE_SIZE, y * TILE_SIZE + (2 * (TILE_SIZE / 3)), TILE_SIZE / 3, TILE_SIZE / 3);
 				g.setColor(PATH_COLOR);
 				if (!maze.rooms[x][y].doors[0].isWall) //LEFT
-					g.fillRect(x * TILE_SIZE, y * TILE_SIZE + (TILE_SIZE / 3), TILE_SIZE / 3, TILE_SIZE / 3);
+					g2d.fill3DRect(x * TILE_SIZE, y * TILE_SIZE + (TILE_SIZE / 3), TILE_SIZE / 3, TILE_SIZE / 3, true);
 				if (!maze.rooms[x][y].doors[1].isWall) //RIGHT
-					g.fillRect(x * TILE_SIZE + (2 * (TILE_SIZE / 3)), y * TILE_SIZE + (TILE_SIZE / 3), TILE_SIZE, TILE_SIZE);
+					g2d.fill3DRect(x * TILE_SIZE + (2 * (TILE_SIZE / 3)), y * TILE_SIZE + (TILE_SIZE / 3), TILE_SIZE, TILE_SIZE, true);
 				if (!maze.rooms[x][y].doors[2].isWall) //UP
-					g.fillRect(x * TILE_SIZE + (TILE_SIZE / 3), y * TILE_SIZE, TILE_SIZE, TILE_SIZE);
+					g2d.fill3DRect(x * TILE_SIZE + (TILE_SIZE / 3), y * TILE_SIZE, TILE_SIZE, TILE_SIZE, true);
 				if (!maze.rooms[x][y].doors[3].isWall) //DOWN
-					g.fillRect(x * TILE_SIZE + (TILE_SIZE / 3), y * TILE_SIZE + (2 * (TILE_SIZE / 3)), TILE_SIZE, TILE_SIZE);
+					g2d.fill3DRect(x * TILE_SIZE + (TILE_SIZE / 3), y * TILE_SIZE + (2 * (TILE_SIZE / 3)), TILE_SIZE, TILE_SIZE, true);
 				//if (currentX == x && currentY == y)
 				//	g.setColor(WALKER_COLOR);
 				//g.fillRect(x * TILE_SIZE + (TILE_SIZE / 3), y * TILE_SIZE + (TILE_SIZE / 3), TILE_SIZE / 3, TILE_SIZE / 3);
 			}
 		}
-		
-	}*/
-	
-	
+	}
 }
